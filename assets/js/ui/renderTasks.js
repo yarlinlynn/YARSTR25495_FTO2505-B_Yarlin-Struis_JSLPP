21e6d4c85a46c2302db7ae502f8124a0f2fb37f4
Yarlin <yarlinlynn@gmail.com>
2025-08-14 14:00:02 +0200

    Add save & delete buttons
    edit tasks with DOM update and remove tasks from DOM and localStorage

--- assets/js/ui/renderTasks.js
+++ assets/js/ui/renderTasks.js
@@ -1,31 +1,49 @@
 // render.js
 import { todoColumn, inProgressColumn, doneColumn, todoHeading, inProgressHeading, doneHeading } from "../script.js";
 import { renderTaskModal } from "../tasks/taskModal.js";
-import { tasks } from "../script";
+import { tasks } from "../script.js";
 
+
+/**
+ * Clear all task columns and reset counts.
+*/
 let todoCount = 0;
 let inProgressCount = 0;
 let doneCount = 0;
 
-/**
- * Clear all task columns and reset counts.
- */
-export function clearTaskColumns() {
-  todoColumn.innerHTML = "";
-  inProgressColumn.innerHTML = "";
-  doneColumn.innerHTML = "";
-  todoCount = inProgressCount = doneCount = 0;
-  todoHeading.textContent = `TODO (0)`;
-  inProgressHeading.textContent = `IN PROGRESS (0)`;
-  doneHeading.textContent = `DONE (0)`;
-}
 
 /**
  * Render a single task to the appropriate column.
  * @typedef {Object} Task
  */
 
-export function renderTasksToTheDom(task) {
+// export function renderTasksToTheDom(tasks) {
+//   const listElement = document.createElement("li");
+//   listElement.className =
+//     "task mb-[21px] bg-white h-[65px] pt-[20px] px-[19px] pb-[21px] rounded-lg text-[15px] font-bold leading-[100%] text-rich-black shadow-custom-shadow cursor-pointer focus:outline-none focus:ring-2 focus:ring-indigo dark:bg-dark-grey dark:text-white";
+//   listElement.setAttribute("tabindex", "0");
+//   listElement.textContent = tasks.title;
+
+//   listElement.addEventListener("click", () => renderTaskModal(tasks));
+
+//   if (tasks.status === "todo") {
+//     todoColumn.appendChild(listElement);
+//     todoCount++;
+//     todoHeading.textContent = `TODO (${todoCount})`;
+//   } else if (tasks.status === "doing") {
+//     inProgressColumn.appendChild(listElement);
+//     inProgressCount++;
+//     inProgressHeading.textContent = `IN PROGRESS (${inProgressCount})`;
+//   } else if (tasks.status === "done") {
+//     doneColumn.appendChild(listElement);
+//     doneCount++;
+//     doneHeading.textContent = `DONE (${doneCount})`;
+//   }
+// }
+
+// import { fetchData } from "../utils/fetchData.js";
+
+export function renderDataToTheDom(task) {
   const listElement = document.createElement("li");
   listElement.className =
     "task mb-[21px] bg-white h-[65px] pt-[20px] px-[19px] pb-[21px] rounded-lg text-[15px] font-bold leading-[100%] text-rich-black shadow-custom-shadow cursor-pointer focus:outline-none focus:ring-2 focus:ring-indigo dark:bg-dark-grey dark:text-white";
@@ -48,4 +66,3 @@ export function renderTasksToTheDom(task) {
     doneHeading.textContent = `DONE (${doneCount})`;
   }
 }
-
