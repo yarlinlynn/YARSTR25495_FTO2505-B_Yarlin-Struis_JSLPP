1ba601084c547cb4c8f8d321cb52a83465a0b028
Yarlin <yarlinlynn@gmail.com>
2025-08-14 16:34:16 +0200

    Removed alert and added loading message animation for fetching data

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,14 +38,31 @@ let doneCount = 0;
 
 let tasks = []
 
+// delay the message
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Show loading message
+function showLoading() {
+  document.getElementById('loading').classList.remove("hidden");
+}
+
+// Hide loading message
+function hideLoading() {
+  document.getElementById('loading').classList.add("hidden");
+}
+
 /**
  * Fetch data from API
 */
-
 fetchData();
 
 async function fetchData() {
-  alert("Fetching data... please wait.");
+  // show loading message while data is being fetched from api
+  // alert("Fetching data... please wait.");
+  showLoading(); // show loading animation
+  await delay(2000); // wait for 2 seconds before fetching
 
   try {
     const response = await fetch("https://jsl-kanban-api.vercel.app/");
@@ -71,6 +88,8 @@ async function fetchData() {
   } catch (error) {
     console.error(error);
     alert("Failed to fetch tasks from API.");
+  } finally {
+    hideLoading(); // hide loading animation after fetch
   }
 }
 
@@ -355,88 +374,6 @@ function mobileNav() {
 /**
  * Render new task modal popup
 */
-// document.querySelectorAll("#newTaskButton").forEach(btn => {
-//   btn.addEventListener("click", function () {
-
-//   // Create a new element for popup Modal asking user to add new task
-//   const modalPopUp = document.createElement("section");
-//   // modalPopUp.classList.add("absolute top-0 w-full ")
-
-//   modalPopUp.innerHTML = `
-//     <!-- Backdrop -->
-//         <div class="bg-black opacity-50 fixed top-0 left-0 w-full h-[130vh]"></div>
-
-//         <div id="formContainer" class="bg-white dark:bg-dark-grey absolute top-[69px] lg:top-[210px] left-0 right-0 m-auto w-[343px] md:w-[600px] p-[24px] rounded-[6px] h-[560px] md:h-[587px] flex flex-col g-[24px]">
-
-//           <div class="flex items-center justify-between pb-4">
-//             <h2 class="text-[18px] dark:text-white font-bold leading-100%">New Task</h2>
-                    
-//             <img src="./assets/images/Group 45.svg" id="closeButton" alt="close button">
-//           </div>
-
-//           <!-- Form Container -->
-//         <form>
-//           <!-- Task Title -->
-//           <label for="title" class="text-[12px] font-bold text-medium-grey">Title</label>
-//           <div class="mt-[8px] relative">
-//             <input id="title" name="title" type="text" placeholder="e.g. Take chilled break" required class="relative dark:text-black font-body border border-gray-300 rounded h-[40px] w-full pt-[8px] pb-[9px] pl-[18px] mb-[24px]  text-[13px]"/>
-//             <!-- Error Message -->
-//             <p id="errorMessage" class="absolute w-[180px] h-[48px] bg-white text-medium-grey rounded-[8px] right-[25px] left-[128px] top-[55px] shadow-custom-shadow pt-[12px] text-[13px] text-center">❗ Please fill out this field.</p>
-//           </div>
-
-//           <!-- Task Description -->
-//           <label for="description" class="text-[12px] font-bold text-medium-grey">Description</label>
-//           <textarea id="description" name="description" placeholder="e.g. Pet your dog, have a cup of coffee, dance to your favourite song and come back to conquer this challenge. 💪🏾" required
-//             class="h-[112px] text-[13px] border border-gray-300 rounded w-full pt-[8px] pb-[9px] pl-[18px] pr-[18px] mb-[24px] mt-[8px] dark:text-black"></textarea>
-
-//           <!-- Task Status -->
-//           <label for="taskStatus" class="mb-[8px] text-[12px] font-bold text-medium-grey">Status</label>
-//           <div class="w-full border border-green-300 rounded flex items-center justify-between mb-[24px] mt-[8px]">
-//             <select id="taskStatus" class="appearance-none font-body text-[13px] w-full pt-[8px] pb-[8px] pl-[18px] leading-[23px] pr-4 bg-[url('data:image/svg+xml;utf8,<svg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%2211%22%20height=%228%22%20viewBox=%220%200%2011%208%22%20fill=%22none%22><path%20d=%22M0.79834%201.54863L5.49682%206.24711L10.1953%201.54863%22%20stroke=%22%23828FA3%22%20stroke-width=%222%22/></svg>')] bg-no-repeat bg-[length:0.8rem] bg-[right_0.75rem_center]
-//           border border-gray-400 rounded px-3 py-2 dark:text-black">
-//               <option value="todo">todo</option>
-//               <option value="doing">in progress</option>
-//               <option value="done">done</option>
-//             </select>
-//           </div>
-
-//           <!-- Task Priority -->
-//           <label for="taskPriority" class="mb-[8px] text-[12px] font-bold text-medium-grey">Priority</label>
-//           <div class="w-full border border-gray-300 rounded flex items-center justify-between mb-[24px] mt-[8px]">
-//             <select id="taskPriority" class="appearance-none font-body text-[13px] w-full pt-[8px] pb-[8px] pl-[18px] leading-[23px] pr-4 bg-[url('data:image/svg+xml;utf8,<svg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%2211%22%20height=%228%22%20viewBox=%220%200%2011%208%22%20fill=%22none%22><path%20d=%22M0.79834%201.54863L5.49682%206.24711L10.1953%201.54863%22%20stroke=%22%23828FA3%22%20stroke-width=%222%22/></svg>')] bg-no-repeat bg-[length:0.8rem] bg-[right_0.75rem_center]
-//           border border-gray-400 rounded px-3 py-2 dark:text-black">
-//               <option value="low">low</option>
-//               <option value="medium">medium</option>
-//               <option value="high">high</option>
-//             </select>
-//           </div>
-
-//           <!-- Form Buttons -->
-//           <button id="createNewTask" class="w-[290px] h-[40px] rounded-[20px] bg-[#635FC7] text-white font-bold text-[13px] leading-[23px] md:w-[424px] md:m-auto md:flex md:items-center md:justify-center">Create Task</button>
-          
-//         </form>
-
-//         </div>
-//   `;
-
-//   document.body.appendChild(modalPopUp);
-
-//   // Add event listener for the submit button to call the getTaskDetails function
-//   document.getElementById("createNewTask").addEventListener("click", function(e) {
-//     e.preventDefault(); // Prevent default form submit
-
-//     const taskCreated = getNewTaskDetails(); 
-//     // check if title is entered from user
-//     if (taskCreated) { // Only close if a task was added
-//       modalPopUp.remove();
-//     }
-//   });
-  
-//   // Removes the whole dynamic content when clicked
-//     modalPopUp.querySelector("#closeButton").addEventListener("click", () => modalPopUp.remove());
-//   });
-// });
-
 newTaskModal()
 function newTaskModal() {
   document.querySelectorAll("#newTaskButton").forEach(btn => {
@@ -523,6 +460,7 @@ function getNewTaskDetails() {
   // calculate the id of task entered by user and have it start at the last id from array
   // let id = tasks[tasks.length - 1 ].id + 1
   let id = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+  // id: tasks.length ? Math.max(...tasks.map((t) => t.id)) + 1 : 1 another way to calculate the id
 
   // get user title, description and status entered
   let title = document.getElementById("title").value.trim();
@@ -610,4 +548,3 @@ document.addEventListener('change', (e) => {
     toggleTheme(e.target.checked);
   }
 });
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,8 @@ module.exports = {
   'gap-[15px]', 'py-[8px]', 'top-[99px]', 'lg:top-[249px]', 'right-0', 'm-auto', 'h-[415px]', 'md:h-[478px]', 'g-[24px]', 'justify-between', "lg:top-[200px]",
   'pb-4', 'h-[112px]', 'rounded-[20px]', 'w-[290px]', 'bg-[#A8A4FF]', 'w-[56px]', 'pt-2', 'pl-3', 'text-[20px]', "md-[43px]", "h-[65px]", "dark:text-black",
   "bg-[url('data:image/svg+xml;utf8,<svg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%2211%22%20height=%228%22%20viewBox=%220%200%2011%208%22%20fill=%22none%22><path%20d=%22M0.79834%201.54863L5.49682%206.24711L10.1953%201.54863%22%20stroke=%22%23828FA3%22%20stroke-width=%222%22/></svg>')]",
-  "dark:text-white", "dark:bg-dark-grey", "dark:bg-indigo", "h-[200px]", "w-[200px]", "bg-[#f5f5dc]", "flex", "m-auto", "fixed", "inset-0", "z-50", "animate-spin", "rounded-full", "block", "h-[50px]", "w-[50px]", "border-t-4", "border-light-grey", "border-solid mb-4"
+  "dark:text-white", "dark:bg-dark-grey", "dark:bg-indigo", "h-[200px]", "w-[200px]", "bg-[#f5f5dc]", "flex", "m-auto", "fixed", "inset-0", "z-50", "animate-spin", "rounded-full", "block", "h-[50px]", "w-[50px]", "border-t-4", "border-light-grey", "border-solid mb-4",
+  "top-1/2", "left-1/2", "-translate-x-1/2", "-translate-y-1/2", "bg-white", "p-5", " rounded-[10px]", "shadow-md", "w-10", "h-10", "border-4", "border-gray-200", "border-t-4", "border-t-blue-500", "rounded-full", "animate-spin", "mx-auto", "mb-4"
 ],
   theme: {
     extend: {
